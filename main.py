4cd6a4d55c98c5205d9a9400b3ca5ecab4c501dc
electricbayan <a.volkovich@internet.ru>
2024-08-17 23:21:32 +0500

    Minor fixes

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import asyncio
 from src.routes.choose_lang import lang_rt
 from src.routes.routes import main_rt
 import logging
+from src.middleware.users import InsertUserMiddleware
+from api.database.database import Database
+from src.setup_commands import commands
 
 
 logging.basicConfig(level=logging.INFO)
@@ -12,7 +15,13 @@ load_dotenv()
 bot = Bot(getenv("TG_TOKEN"))
 
 async def main():
+    logging.basicConfig(level=logging.INFO)
+    await Database.create_tables()
+    bot = Bot(getenv("TG_TOKEN"))
+    await bot.set_my_commands(commands=commands)
+
     dp = Dispatcher()
+    dp.callback_query.middleware(InsertUserMiddleware())
     dp.include_routers(
         lang_rt, main_rt    
     )
---
+++ src/middleware/users.py
@@ -0,0 +1,15 @@
+from aiogram import BaseMiddleware
+from aiogram.types import CallbackQuery
+# from api.database.database import Database
+
+class InsertUserMiddleware(BaseMiddleware):
+    # db = Database()
+    async def __call__(self, handler,
+        event: CallbackQuery,
+        data):
+        # if event.data == "ru":
+        #     await self.db.insert_user(tg_id=str(event.from_user.id), lang="ru")
+        # elif event.data == "en":
+        #     await self.db.insert_user(tg_id=str(event.from_user.id), lang="en")
+
+        await handler(event, data)
